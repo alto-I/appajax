ac8f099a325e8635baa2a033d94250dcaf8e5ee7
alto-I <blmaldiy@gmail.com>
2021-12-07 11:32:33 +0900

    fetchのPromise部分をasync/awaitに変えた

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then((response) => {
-      console.log(response.status);
-      if (!response.ok) {
-        console.log("エラーレスポンス", response);
-      } else {
-        return response.json().then((userInfo) => {
-          console.log(userInfo);
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+async function fetchUserInfo(userId) {
+  console.log("--- start ---");
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${encodeURIComponent(userId)}`
+    );
+    console.log("レスポンスステータス:", response.status);
+    if (!response.ok) {
+      console.log("エラーレスポンス", response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("--- end ---");
+  }
 }
